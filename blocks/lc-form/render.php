d935d6a4acd9036381720c4c7336cdda09b80188
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2026-09-17 11:17:25 +0100

    Add link to Google reviews in contact form section

--- blocks/lc-form/render.php
+++ blocks/lc-form/render.php
@@ -16,6 +16,7 @@ $wrapper_attributes = get_block_wrapper_attributes( array( 'class' => 'form-bloc
 		<div class="has-600-font-size mb-4">
 			<p class="mb-3">Call us on <?php echo do_shortcode( '[contact_phone]' ); ?>, or message on <?php echo do_shortcode( '[whatsapp_link]' ); ?>.</p>
 			<p>Prefer not to call? Fill in the form below and we'll get back to you fast.</p>
+			<p><a href="https://maps.app.goo.gl/NQfZEeBUGsMdfy8E6">Read our reviews on Google</a></p>
 		</div>
 		<div class="form-card">
 			<?php echo do_shortcode( '[contact-form-7 id="' . esc_attr( $contact_form_id ) . '"]' ); ?>
